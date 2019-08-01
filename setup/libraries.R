ec6ad5f181b4f20580d6d3624d6a00d73a05ee3f
Gflemin <glflemin@Grants-MacBook-Pro.local>
2019-08-01 13:52:32 -0400

    Fixed a bug where the xgboost, BH, and plogr packages weren't installed as well as another bug with packrat

--- setup/libraries.R
+++ setup/libraries.R
@@ -1,10 +1,14 @@
 ### NOTE: 
 # Packages for using markdown/R notebooks should be installed by interacting with with the notebook document
 
-# Install the required packages
+# Install the required packages if packrat fails for some reason
 # packages = c("drake", "visNetwork", "readr", "dplyr", "recipes", "yardstick", "mlr", "ggplot2", "stringr", "here",
-#            "rsample", "iml", "randomForest", "e1071", "tidyr", "tibble", "devtools", "iml")
+#            "rsample", "iml", "randomForest", "xgboost", "e1071", "tidyr", "tibble", "devtools", "iml",
+#            "BH",  "plogr")
 # install.packages(packages)
+
+# Restore packrat packages
+packrat::restore()
 
 # Load the packages
 library(drake)
@@ -26,6 +30,8 @@ library(tidyr)
 library(tibble)
 library(devtools)
 library(iml)
+library(BH)
+library(plogr)
 
 # Setting paths with here
 functions = here("setup", "functions.R")
